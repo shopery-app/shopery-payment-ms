4c19b1d4d99f8645a8c2e5a6b5d9f7dfc52e1dde
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-27 16:11:31 +0400

    project setup

---
+++ src/main/java/az/shopery/payment_ms/PaymentMsApplication.java
@@ -0,0 +1,13 @@
+package az.shopery.payment_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/shopery/payment_ms/PaymentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery.payment_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
